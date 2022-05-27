d7791a55aa004fe8f9612df237996a68221b3ed2
neetha <neethacohort@gmail.com>
2022-05-26 19:58:26 -0600

    added SignUp form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
-import './App.css';
-import Navigation from './Components/Navigation';
-import Footer from './Components/Footer';
+import "./App.css";
+import Navigation from "./Components/Navigation";
+import Footer from "./Components/Footer";
 import { Routes, Route } from "react-router-dom";
-import Home from './Pages/Home';
+import Home from "./Pages/Home";
+import Login from "./Components/Login";
+import SignUp from "./Components/SignUp";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Routes>
-        <Route path='/' element={<Home />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
       <Footer />
     </div>
