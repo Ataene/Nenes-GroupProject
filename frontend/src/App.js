cd1cd06c7fc1b63a35191accb69ee00f61419927
Alafonye K. Emmanuel <ke.alafonye@yahoo.com>
2022-06-01 19:56:55 -0600

    Error/Conflicts Resolved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Navigation from "./Components/Navigation";
 import Footer from "./Components/Footer";
 import { Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
 import Home from './Pages/Home';
 import About from './Pages/About';
 import ProductPage from './Pages/ProductPage';
@@ -13,15 +12,6 @@ import SignUp from "./Components/SignUp";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import {useAuth} from "./auth/useAuth";
 
-=======
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import ProductPage from "./Pages/ProductPage";
-import Dashboard from "./Components/Dashboard";
-import ProductDetail from "./Components/ProductDisplay/ProductDetail";
-import Login from "./Components/Login";
-import SignUp from "./Components/SignUp";
->>>>>>> c752cc006c6325735e1b6c83b4beb9d057a18ffd
 
 function App() {
   
@@ -34,11 +24,7 @@ function App() {
         <Route path="/products" element={<ProductPage />} />
         <Route path="/about" element={<About />} />
         <Route path="/product/:id" element={<ProductDetail />} />
-<<<<<<< HEAD
         <Route path="/dashboard" element={<ProtectedRoute isAuthed={!!user} isLoading={isLoading} element={<Dashboard />} />} />
-=======
-        <Route path="/dashboard" element={<Dashboard />} />
->>>>>>> c752cc006c6325735e1b6c83b4beb9d057a18ffd
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
--- frontend/src/Components/Navigation.jsx
+++ frontend/src/Components/Navigation.jsx
@@ -2,15 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-<<<<<<< HEAD
 import { Toolbar, Typography, Container, Button } from "@mui/material";
 import LoginIcon from '@mui/icons-material/Login';
-=======
-import { Toolbar, IconButton, Typography, Container, Button } from "@mui/material";
-import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
-import LoginIcon from '@mui/icons-material/Login';
-
->>>>>>> c752cc006c6325735e1b6c83b4beb9d057a18ffd
 
 const Navigation = () => {
   return (
@@ -52,11 +45,7 @@ const Navigation = () => {
                 </Button>
               </Link>
             </Box>
-<<<<<<< HEAD
             <Box style={{ textDecoration: "none" }} sx={{ flexGrow: 0, display: { xs: "none", md: "flex" } }}>
-=======
-            <Box sx={{ display: { xs: "none", md: "flex" } }}>
->>>>>>> c752cc006c6325735e1b6c83b4beb9d057a18ffd
               <Link style={{ textDecoration: "none" }} to="/login">
                 <Button sx={{ my: 2, color: "white", alignItem: "center" }}>
                   <LoginIcon />Login
