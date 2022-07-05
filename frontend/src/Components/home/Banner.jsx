2d579f1f9a3b1e0b84d4cd029bf00ee357bae6cc
Nicole <nicoleamdubois@hotmail.com>
2022-07-04 22:18:03 -0600

    Edited Banner message

--- frontend/src/Components/home/Banner.jsx
+++ frontend/src/Components/home/Banner.jsx
@@ -41,8 +41,8 @@ export default function Banner() {
           variant="subtitle"
           sx={{ fontFamily: "Montserrat", fontSize: "19px" }}
         >
-          Find things you need on Hundie and trade them for items you own and
-          would love exchange.
+          Find things you need on Hundie, trade them for items you own and
+          would love exchange!
         </BannerDescription>
 
         <BannerShopButton
