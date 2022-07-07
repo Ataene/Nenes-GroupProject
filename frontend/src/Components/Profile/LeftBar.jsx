7a4c6f90102311a8f328383f0dd32c1f9845fdb7
Neetha <neethacohort@gmail.com>
2022-07-07 08:20:02 -0600

    added size

--- frontend/src/Components/Profile/LeftBar.jsx
+++ frontend/src/Components/Profile/LeftBar.jsx
@@ -1,87 +1,98 @@
 import React, { useState } from "react";
-import { Box, Container, List, ListItemButton, ListItemIcon, ListItemText, Card } from "@mui/material";
+import {
+  Box,
+  Container,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Card,
+} from "@mui/material";
 import RssFeedIcon from "@mui/icons-material/RssFeed";
 import ChatIcon from "@mui/icons-material/Chat";
 import InventoryIcon from "@mui/icons-material/Inventory";
-import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import LiveHelpIcon from '@mui/icons-material/LiveHelp';
-import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
-import WorkIcon from '@mui/icons-material/Work';
-import StorefrontIcon from '@mui/icons-material/Storefront';
-import GroupsIcon from '@mui/icons-material/Groups';
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import LiveHelpIcon from "@mui/icons-material/LiveHelp";
+import CircleNotificationsIcon from "@mui/icons-material/CircleNotifications";
+import WorkIcon from "@mui/icons-material/Work";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import GroupsIcon from "@mui/icons-material/Groups";
 
-const LeftBar = () => {  
-  const [ active, setActive ] = useState('');
-
+const LeftBar = () => {
+  const [active, setActive] = useState("");
 
   return (
     <>
-      <Box sx={{ 
-        flex: "1.5", 
-        // backgroundColor: "#A5BECC", 
-        position: "sticky", 
-        maxHeight: "auto"
-      }}>
+      <Box
+        sx={{
+          flex: "1.5",
+          // backgroundColor: "#A5BECC",
+          position: "sticky",
+          maxHeight: "auto",
+          display: { xs: "none", md: "flex" },
+        }}
+      >
         <Card>
           <List component="nav" aria-labelledby="nested-list-subheader">
             <ListItemButton>
               <ListItemIcon>
-                <RssFeedIcon sx={{ color: "#B8F1B0"}} />
+                <RssFeedIcon sx={{ color: "#B8F1B0" }} />
               </ListItemIcon>
               <ListItemText primary="Sent mail" />
             </ListItemButton>
             <ListItemButton>
               <ListItemIcon>
-                <ChatIcon sx={{ color: "#B8F1B0"}} />
+                <ChatIcon sx={{ color: "#B8F1B0" }} />
               </ListItemIcon>
               <ListItemText primary="Drafts" />
             </ListItemButton>
             <ListItemButton>
               <ListItemIcon>
-                <GroupsIcon sx={{ color: "#B8F1B0"}} />
+                <GroupsIcon sx={{ color: "#B8F1B0" }} />
               </ListItemIcon>
               <ListItemText primary="Groups" />
             </ListItemButton>
             <ListItemButton>
               <ListItemIcon>
-                <StorefrontIcon sx={{ color: "#B8F1B0"}} />
+                <StorefrontIcon sx={{ color: "#B8F1B0" }} />
               </ListItemIcon>
               <ListItemText primary="Market" />
             </ListItemButton>
             <ListItemButton>
               <ListItemIcon>
-                <InventoryIcon sx={{ color: "#B8F1B0"}} />
+                <InventoryIcon sx={{ color: "#B8F1B0" }} />
               </ListItemIcon>
               <ListItemText primary="Inbox" />
-            </ListItemButton><hr />
+            </ListItemButton>
+            <hr />
             <ListItemButton>
               <ListItemIcon>
-                <WorkIcon sx={{ color: "#B8F1B0"}} />
+                <WorkIcon sx={{ color: "#B8F1B0" }} />
               </ListItemIcon>
               <ListItemText primary="Career" />
             </ListItemButton>
             <ListItemButton>
               <ListItemIcon>
-                <CircleNotificationsIcon sx={{ color: "#B8F1B0"}} />
+                <CircleNotificationsIcon sx={{ color: "#B8F1B0" }} />
               </ListItemIcon>
               <ListItemText primary="Notify" />
             </ListItemButton>
             <ListItemButton>
               <ListItemIcon>
-                <LiveHelpIcon sx={{ color: "#B8F1B0"}} />
+                <LiveHelpIcon sx={{ color: "#B8F1B0" }} />
               </ListItemIcon>
               <ListItemText primary="FAQ" />
             </ListItemButton>
             <ListItemButton>
               <ListItemIcon>
-                <CalendarMonthIcon sx={{ color: "#B8F1B0"}} />
+                <CalendarMonthIcon sx={{ color: "#B8F1B0" }} />
               </ListItemIcon>
               <ListItemText primary="Event" />
             </ListItemButton>
             <ListItemButton>
               <ListItemIcon>
-                <OndemandVideoIcon sx={{ color: "#B8F1B0"}} />
+                <OndemandVideoIcon sx={{ color: "#B8F1B0" }} />
               </ListItemIcon>
               <ListItemText primary="Video" />
             </ListItemButton>
--- frontend/src/Components/Profile/Profile.jsx
+++ frontend/src/Components/Profile/Profile.jsx
@@ -99,8 +99,11 @@ const Profile = () => {
 
   return (
     <>
-      {/* <Paper elevation={10} sx={{ backgroundColor: "#B8F1B0" }}> */}
-      {/* <br />
+      <Paper
+        elevation={10}
+        sx={{ backgroundColor: "#B8F1B0", height: "30rem" }}
+      >
+        <br />
         <Typography sx={{ display: "flex", justifyContent: "center" }}>
           UPDATE ACCOUNT
         </Typography>
@@ -112,150 +115,150 @@ const Profile = () => {
           }}
           noValidate
           autoComplete="off"
-        > */}
-      <form
-        style={{
-          height: "300px",
-          width: "100%",
-          margin: "5px",
-          paddingLeft: "70px",
-        }}
-        onSubmit={handleSubmit}
-      >
-        <section>
-          <TextField
-            id="outlined-multiline-flexible"
-            label="First Name"
-            variant="outlined"
-            type="text"
-            name="firstName"
-            required={true}
-            value={formData.firstName}
-            onChange={handleChange}
-          />
-
-          <TextField
-            id="outlined-basic"
-            label="Last Name"
-            variant="outlined"
-            type="text"
-            name="lastName"
-            required={true}
-            value={formData.lastName}
-            onChange={handleChange}
-          />
-        </section>
-        <section>
-          <TextField
-            id="outlined-basic"
-            label="Gender"
-            variant="outlined"
-            type="text"
-            name="gender"
-            required={true}
-            value={formData.gender}
-            onChange={handleChange}
-          />
-
-          <TextField
-            id="outlined-basic"
-            label="Occupation"
-            variant="outlined"
-            type="text"
-            name="occupation"
-            required={true}
-            value={formData.occupation}
-            onChange={handleChange}
-          />
-        </section>
-        <section>
-          <TextField
-            iid="outlined-basic"
-            label="City"
-            variant="outlined"
-            type="text"
-            name="city"
-            required={true}
-            value={formData.city}
-            onChange={handleChange}
-          />
-
-          <TextField
-            id="outlined-basic"
-            label="Province"
-            variant="outlined"
-            type="text"
-            name="province"
-            required={true}
-            value={formData.province}
-            onChange={handleChange}
-          />
-        </section>
-        <section>
-          <TextField
-            id="outlined-basic"
-            label="Postal Code"
-            variant="outlined"
-            type="text"
-            name="postalCode"
-            required={true}
-            value={formData.postalCode}
-            onChange={handleChange}
-            inputProps={{ maxLength: 3 }}
-          />
-        </section>
-
-        <section
-          style={{
-            transform: "translate(-30%, -60%)",
-            position: "absolute",
-            top: "65%",
-            left: "62%",
-          }}
         >
-          <Avatar
-            src={
-              file ? (
-                URL.createObjectURL(file)
-              ) : (
-                <Typography>Image failed to load</Typography>
-              )
-            }
-            sx={{
-              bgcolor: "green"[500],
-              width: 90,
-              height: 100,
-              marginTop: "30px",
-            }}
-            variant="rounded"
-          />
-          <input
-            id="standard-basic"
-            label="Profile Photo"
-            variant="standard"
-            type="file"
-            name="url"
-            required={true}
-            value={formData.url}
-            onChange={handleFileChange}
-          />
-          <br />
-          <br />
-          <Button
+          <form
             style={{
-              paddingTop: "20px",
-              transform: "translate(-280%, 140%)",
+              height: "300px",
+              width: "100%",
+              margin: "5px",
+              paddingLeft: "70px",
             }}
-            disabled={progress !== null && progress < 100}
-            type="submit"
+            onSubmit={handleSubmit}
           >
-            Submit
-          </Button>
-          {progress ? <div>progress: {progress}%</div> : <div />}
-        </section>
-      </form>
-      {/* </Box> */}
-      {/* </Paper> */}
+            <section>
+              <TextField
+                id="outlined-multiline-flexible"
+                label="First Name"
+                variant="outlined"
+                type="text"
+                name="firstName"
+                required={true}
+                value={formData.firstName}
+                onChange={handleChange}
+              />
+
+              <TextField
+                id="outlined-basic"
+                label="Last Name"
+                variant="outlined"
+                type="text"
+                name="lastName"
+                required={true}
+                value={formData.lastName}
+                onChange={handleChange}
+              />
+            </section>
+            <section>
+              <TextField
+                id="outlined-basic"
+                label="Gender"
+                variant="outlined"
+                type="text"
+                name="gender"
+                required={true}
+                value={formData.gender}
+                onChange={handleChange}
+              />
+
+              <TextField
+                id="outlined-basic"
+                label="Occupation"
+                variant="outlined"
+                type="text"
+                name="occupation"
+                required={true}
+                value={formData.occupation}
+                onChange={handleChange}
+              />
+            </section>
+            <section>
+              <TextField
+                iid="outlined-basic"
+                label="City"
+                variant="outlined"
+                type="text"
+                name="city"
+                required={true}
+                value={formData.city}
+                onChange={handleChange}
+              />
+
+              <TextField
+                id="outlined-basic"
+                label="Province"
+                variant="outlined"
+                type="text"
+                name="province"
+                required={true}
+                value={formData.province}
+                onChange={handleChange}
+              />
+            </section>
+            <section>
+              <TextField
+                id="outlined-basic"
+                label="Postal Code"
+                variant="outlined"
+                type="text"
+                name="postalCode"
+                required={true}
+                value={formData.postalCode}
+                onChange={handleChange}
+                inputProps={{ maxLength: 3 }}
+              />
+            </section>
+
+            <section
+              style={{
+                transform: "translate(-30%, -60%)",
+                position: "absolute",
+                top: "45%",
+                left: "62%",
+              }}
+            >
+              <Avatar
+                src={
+                  file ? (
+                    URL.createObjectURL(file)
+                  ) : (
+                    <Typography>Image failed to load</Typography>
+                  )
+                }
+                sx={{
+                  bgcolor: "green"[500],
+                  width: 90,
+                  height: 100,
+                  marginTop: "30px",
+                }}
+                variant="rounded"
+              />
+              <input
+                id="standard-basic"
+                label="Profile Photo"
+                variant="standard"
+                type="file"
+                name="url"
+                required={true}
+                value={formData.url}
+                onChange={handleFileChange}
+              />
+              <br />
+              <br />
+              <Button
+                style={{
+                  paddingTop: "20px",
+                  transform: "translate(-280%, 280%)",
+                }}
+                disabled={progress !== null && progress < 100}
+                type="submit"
+              >
+                Submit
+              </Button>
+              {progress ? <div>progress: {progress}%</div> : <div />}
+            </section>
+          </form>
+        </Box>
+      </Paper>
     </>
   );
 };
