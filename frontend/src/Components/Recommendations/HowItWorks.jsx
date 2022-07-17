7ac279f47de397989cd62408acc567d86ba5e2fb
Nicole <nicoleamdubois@hotmail.com>
2022-07-17 11:01:59 -0600

    added map pic

--- frontend/src/Components/Recommendations/HowItWorks.jsx
+++ frontend/src/Components/Recommendations/HowItWorks.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { Box, Container, Typography } from "@mui/material";
-import Paper from '@mui/material/Paper';
-import Stack from '@mui/material/Stack';
-import { styled } from '@mui/material/styles';
-import EditLocationOutlinedIcon from '@mui/icons-material/EditLocationOutlined';
-import CarCrashOutlinedIcon from '@mui/icons-material/CarCrashOutlined';
-import LockClockOutlinedIcon from '@mui/icons-material/LockClockOutlined';
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+import EditLocationOutlinedIcon from "@mui/icons-material/EditLocationOutlined";
+import CarCrashOutlinedIcon from "@mui/icons-material/CarCrashOutlined";
+import LockClockOutlinedIcon from "@mui/icons-material/LockClockOutlined";
 
 const HowItWorks = () => {
-
   const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
     ...theme.typography.body2,
     padding: theme.spacing(12),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
   }));
   return (
@@ -21,29 +20,54 @@ const HowItWorks = () => {
       <Box>
         <Container>
           <div>
-          <Typography sx={{display: "flex", justifyContent: "center", alignItems: 'center', marginBottom: 3}}>How it Works</Typography>
+            <Typography
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                marginBottom: 3,
+              }}
+            >
+              How it Works
+            </Typography>
             <Stack
               direction={{ xs: "column", sm: "row" }}
               spacing={{ xs: 1, sm: 2, md: 4 }}
-              sx={{display: "flex", justifyContent: "center", alignItems: 'center'}}
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
             >
-              <Item style={{
-            backgroundImage: "url(/images/banner/banner.png)",
-            backgroundSize:"cover",
-
-          }}>
-                <div style= {{backgroundColor: "rgba(255,255,255,0.75)"}}>
-                <EditLocationOutlinedIcon sx={{color: "green"}} />
-              <Typography>Sign up on the hompage then login. </Typography>
-                  </div>
+              <Item
+                style={{
+                  backgroundImage: "url(/images/banner/banner.png)",
+                  backgroundSize: "cover",
+                }}
+              >
+                <div style={{ backgroundColor: "rgba(255,255,255,0.75)" }}>
+                  <EditLocationOutlinedIcon sx={{ color: "green" }} />
+                  <Typography>Sign up on the hompage then login. </Typography>
+                </div>
+              </Item>
+              <Item
+                style={{
+                  backgroundImage: "url(/images/map.png)",
+                  backgroundSize: "cover",
+                }}
+              >
+                <CarCrashOutlinedIcon sx={{ color: "red" }} />
+                <Typography>
+                  Once Logged in, you can see posts within your neigbourhood.{" "}
+                </Typography>
               </Item>
               <Item>
-              <CarCrashOutlinedIcon sx={{color: "red"}}/>
-              <Typography>Once Logged in, you can see posts within your neigbourhood. </Typography>
-              </Item>
-              <Item>
-              <LockClockOutlinedIcon sx={{color: "yellow", size: "large"}}/>
-              <Typography>chat with people nearby to arrange a meet and exchange items. </Typography>
+                <LockClockOutlinedIcon
+                  sx={{ color: "yellow", size: "large" }}
+                />
+                <Typography>
+                  chat with people nearby to arrange a meet and exchange items.{" "}
+                </Typography>
               </Item>
             </Stack>
           </div>
